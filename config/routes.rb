9ed1700778ec5dfa11d7eb4ccb11bb28d135e716
Karishma Jogoo <karishma_jogoo@hotmail.com>
2022-05-24 19:47:39 +0400

    routes_added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show create new] do
+    resources :products, only: %i[index create new show edit update]
+    member do
+      resources :bookings, only: %i[index show create new]
+      resources :reviews, only: %i[show create new]
+    end
+  end
+  resources :products, only: [:delete]
 end
