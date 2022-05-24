a342bbd8cf31741fa8963cd937d4730bf686d746
Cedric Marie <djced96@gmail.com>
2022-05-24 21:58:44 +0400

    CRUD for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+
+# for admin (super-user) only
+  def index
+    @users = User.all
+  end
+
+  # for sign-up
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  # User view his only own profile.
+  # May have to use "if" statement to prevent user from accessing other user's profile.
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  # only the user can delete his account. if statement is a MUST
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :address, :phone)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
