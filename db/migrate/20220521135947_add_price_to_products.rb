68d7df7d07340d30a70ac0b2910f1deee894d332
Karishma Jogoo <karishma_jogoo@hotmail.com>
2022-05-26 19:38:22 +0400

    Add more variables to product

---
+++ db/migrate/20220521135947_add_price_to_products.rb
@@ -0,0 +1,8 @@
+
+class AddPriceToProduct < ActiveRecord::Migration[6.1]
+  def change
+    add_column :products, :price, :integer
+    add_column :products, :quantity, :integer
+    add_column :products, :photo, :text
+  end
+end
