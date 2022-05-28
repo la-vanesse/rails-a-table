e44659f20cd9521d495781544aa37cfe05bd7086
vanessa figaro <vfigaro02@gmail.com>
2022-05-28 15:45:15 +0400

    Cloudinary

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  # Hack thanks to Cloudinary
+  has_many_attached :photos
+
+  # Added validations - don't hesitate to modify/correct it :)
+  validates :name, presence: true, uniqueness: true
+  validates :event_type, presence: true
+  validates :product_type, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
