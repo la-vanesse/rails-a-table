2b7eef757755ac07ad05d9d0e50edefe0021aeca
Karishma Jogoo <karishma_jogoo@hotmail.com>
2022-05-29 21:31:52 +0400

    Seeding modified and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,90 +6,83 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 puts "cleaning up database..."
 Product.destroy_all
+User.destroy_all
 puts "Database cleaned"
+
+user_1 = User.create!(
+  name: "Jack32",
+  email: "Jack32@gmail.com",
+  address: "Pamplemousses",
+  phone: "7315558"
+)
+
+user_2 = User.create!(
+  name: "Gerald",
+  email: "gerald@gmail.com",
+  address: "Chemin Grenier",
+  phone: "7316558"
+)
+
+user_3 = User.create!(
+  name: "Alphose",
+  email: "alph@gmail.com",
+  address: "Blue bay",
+  phone: "7316548"
+)
 
 product_1 = Product.create!(
   name: "Renaissance",
   description: "Steel cutlery with floral decorations",
   event_type: "Birthday",
   product_type: "Cutlery",
-  user: "Alphonse",
-  rating: 2,
+  user_id: "3",
+  price: 10,
   quantity: 48,
-  unit_price: 10)
+  is_booked: false
+)
+
+# product_1.photo.attach(io: file, filename: 'flowery cutlery.jpg', content_type: 'image/jpg')
 
 product_2 = Product.create!(
   name: "Azalea",
   description: "Spice up the image of your dining table by opting for the gold-rimmed Verde dinner set. The vibrant jewel tones of gold and emerald green will make your meal presentation more exciting.",
   event_type: "Party",
   product_type: "Plate",
-  user: "Gerald",
-  rating: 5,
+  user_id: "2",
+  price: 15,
   quantity: 24,
-  unit_price: 15)
+  is_booked: true
+)
+
+# product_2.photo.attach(io: file, filename: 'gold-rimmed verde plates.jpg', content_type: 'image/jpg')
 
 product_3 = Product.create!(
   name: "Venetto",
   description: "Steel cutlery with floral decorations",
   event_type: "wedding",
   product_type: "Glasses",
-  user: "Alphonse",
-  rating: 4,
+  user_id: "3",
+  price: 12,
   quantity: 48,
-  unit_price: 12)
+  is_booked: false
+)
+
+# product_3.photo.attach(io: file, filename: 'crystal glass.jpg', content_type: 'image/jpg')
 
 product_4 = Product.create!(
   name: "Farfetch",
   description: "White, grey and blue Orquestra dinner plate from Vista Alegre featuring a geometric pattern, a round shape and a shallow design.",
   event_type: "Birthday",
   product_type: "Plate",
-  user: "Jack32",
-  rating: 1,
+  user_id: "1",
+  price: 14,
   quantity: 12,
-  unit_price: 14)
+  is_booked: false
+)
 
-product_5 = Product.create!(
-  name: "Franky",
-  description: "Simplicity and utility in white stacking porcelain.",
-  event_type: "christmas",
-  product_type: "Set",
-  user: "Alphonse",
-  rating: 2,
-  quantity: 24,
-  unit_price: 50)
+# product_4.photo.attach(io: file, filename: 'geometric dinner set', content_type: 'image/jpg')
 
-product_6 = Product.create!(
-  name: "Ikeha",
-  description: "Make the most of them by serving up good food on this simple, timeless dinnerware.",
-  event_type: "Party",
-  product_type: "Plate",
-  user: "Jessica",
-  rating: 3,
-  quantity: 12,
-  unit_price: 11)
-
-product_7 = Product.create!(
-  name: "Wahoo",
-  description: "Serve Food in Different Style! Enhance the look of your dining table with this Designer Dinner Set, ideal for storing and serving needs.",
-  event_type: "Birthday",
-  product_type: "set",
-  user: "Jojo",
-  rating: 5,
-  quantity: 24,
-  unit_price: 55)
-
-product_8 = Product.create!(
-  name: "Donald",
-  description: "constructed by premium quality rust-resistant stainless steel, durable and healthy for everyday use, dishwasher safe.",
-  event_type: "Wedding",
-  product_type: "set",
-  user: "Anick",
-  rating: 5,
-  quantity: 24,
-  unit_price: 45)
-
-puts "created #{product.count} products"
+puts "created #{Product.count} products"
 puts "done"
